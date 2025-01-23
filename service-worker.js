f70ca008f1b4257d0bc2af37bfa75d85eda5e0c0
callmemahdi01 <mr1451493@gmail.com>
2025-01-23 16:52:48 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "todo-list-cache-v1";
+const CACHE_NAME = "todo-list-cache-v1.3";
 const urlsToCache = [
     "./",
     "./index.html",
@@ -7,38 +7,49 @@ const urlsToCache = [
     "./style/dark.css",
     "./style/toggle.css",
     "./img/icon-192x192.png",
-    "./img/icon-512x512.png",
-];
+    "./img/icon-512x512.png"
+].map(url => new Request(url, {credentials: 'same-origin'}));
 
-self.addEventListener("install", (event) => {
+async function cacheFirst(request) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || networkFirst(request);
+}
+
+async function networkFirst(request) {
+    try {
+        const response = await fetch(request);
+        if (response && response.status === 200 && response.type === 'basic') {
+            const cache = await caches.open(CACHE_NAME);
+            cache.put(request, response.clone());
+        }
+        return response;
+    } catch (error) {
+        return caches.match(request);
+    }
+}
+
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache);
-        })
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
     );
 });
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        fetch(event.request)
-            .then((response) => {
-                // کپی پاسخ برای ذخیره در کش
-                const responseClone = response.clone();
-                caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(event.request, responseClone);
-                });
-                return response;
-            })
-            .catch(() => {
-                // در صورت عدم دسترسی به شبکه، از کش استفاده کن
-                return caches.match(event.request).then((response) => {
-                    if (response) {
-                        return response;
-                    } else if (event.request.mode === "navigate") {
-                        // اگر درخواست برای صفحه‌ای است که در کش نیست، صفحه پیش‌فرض را باز کن
-                        return caches.match("./index.html");
+self.addEventListener('fetch', event => {
+    event.respondWith(cacheFirst(event.request));
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
                     }
-                });
-            })
+                })
+            );
+        })
     );
 });
